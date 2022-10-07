7c89c6138f3ebfbb7d3196e7cb34925f5ec819d8
clacerda <claudiovieira_jr@hotmail.com>
2022-10-07 19:59:33 -0300

    Aula 1 - CSS com react

---
+++ CSS_REACT/css/src/App.js
@@ -0,0 +1,16 @@
+ 
+import './App.css';
+import MyComponent from './components/MyComponent';
+
+function App() {
+  return (
+    <div className="App">
+      {/* CSS Global */}
+       <h1>React com CSS</h1>
+      {/* CSS de componente */}
+       <MyComponent/>
+    </div>
+  );
+}
+
+export default App;
---
+++ CSS_REACT/css/src/components/MyComponent.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./MyComponent.css"; 
+
+const MyComponent = () => {
+  return (
+    <div> 
+     <h1>CSS de Componente</h1>
+     <MyComponent />
+    </div>
+  )
+}
+
+export default MyComponent
