dcba1c6d275ca08c0b9880e7281c544277d26a9d
clacerda <claudiovieira_jr@hotmail.com>
2022-10-08 17:37:53 -0300

    CSS Inline e Component

--- CSS_REACT/css/src/App.js
+++ CSS_REACT/css/src/App.js
@@ -8,7 +8,11 @@ function App() {
       {/* CSS Global */}
        <h1>React com CSS</h1>
       {/* CSS de componente */}
-       <MyComponent/>
+      <MyComponent />
+      <p>Este parágrafo é do App.js</p>
+
+      {/* CSS inline */}
+      <p style={{color: "white", padding: "25px", borderTop: "2px dotted red"}}>Este elemento foi estilizado de forma inline</p>
     </div>
   );
 }
--- CSS_REACT/css/src/components/MyComponent.js
+++ CSS_REACT/css/src/components/MyComponent.js
@@ -1,11 +1,12 @@
-import React from 'react'
+
 import "./MyComponent.css"; 
 
 const MyComponent = () => {
   return (
     <div> 
      <h1>CSS de Componente</h1>
-     <MyComponent />
+     <p>Este parágrafo é do componente</p>
+     <p className="my-comp-paragraph">Este também é do componente</p>
     </div>
   )
 }
