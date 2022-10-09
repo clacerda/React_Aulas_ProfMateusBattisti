baae8fd37f42ae87165dc10a41340908f04fcf70
clacerda <claudiovieira_jr@hotmail.com>
2022-10-09 09:10:55 -0300

    Desafio de conclusão - 4 - parte 2

--- CSS_REACT/css/src/App.js
+++ CSS_REACT/css/src/App.js
@@ -1,8 +1,13 @@
- 
 import './App.css';
 import MyComponent from './components/MyComponent';
+import {useState} from 'react'
+
 
 function App() {
+
+  const n = 15;
+  const [name] = useState("Odin")
+
   return (
     <div className="App">
       {/* CSS Global */}
@@ -13,6 +18,10 @@ function App() {
 
       {/* CSS inline */}
       <p style={{color: "white", padding: "25px", borderTop: "2px dotted red"}}>Este elemento foi estilizado de forma inline</p>
+
+      {/* CSS inline dinâmico*/}
+      <h2 style={n < 16 ? ({color:"purple"}) : ({color:"green"})}>CSS dinâmico com texto</h2>
+      <h2 style={name === "Odin" ? ({color:"yellow", backgroundColor: '#000'}) : null}>CSS dinâmico com texto</h2>
     </div>
   );
 }
