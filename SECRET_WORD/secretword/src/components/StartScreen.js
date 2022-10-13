4f1989b2a7e38d8872b87ffbfa4184233b44516e
clacerda <claudiovieira_jr@hotmail.com>
2022-10-13 20:09:48 -0300

    New Prj - Secret Word - A game create with React

---
+++ SECRET_WORD/secretword/src/components/StartScreen.js
@@ -0,0 +1,13 @@
+import './StartScreen.css'
+
+const StartScreen = () => {
+  return (
+     <div className="start">
+        <h1>Secret word</h1>
+        <p>Clique no botão abaixo para iniciar o jogo:</p>
+        <button>Comece o jogo</button>
+     </div>
+  )
+}
+
+export default StartScreen
