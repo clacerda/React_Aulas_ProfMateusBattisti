af9c90059f938e6a45e4b11304eaf7ae155dc42d
clacerda <claudiovieira_jr@hotmail.com>
2022-10-19 19:56:34 -0300

    inclusão de dados via POST

--- REQ_HTTP/httpreact/src/App.js
+++ REQ_HTTP/httpreact/src/App.js
@@ -4,6 +4,8 @@
 function App() {
 
   const [products, setProducts] = useState([]);
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
 
   const url = "http://localhost:3000/products";
 
@@ -19,6 +21,34 @@ function App() {
      fetchData();
 
   }, []);
+
+  // 2 - add produtos
+
+  const handleSubmit = async (event) => {
+
+        event.preventDefault();
+
+        const product = {
+          name,
+          price,
+        };
+
+        const res = await fetch(url, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(product),
+        });
+        
+        // 3 - Carregamento dinâmico
+        const addedProduct = await res.json();
+
+        setProducts((prevProducts) => [...prevProducts, addedProduct]);
+
+        setName("");
+        setPrice("");
+  };
  
 
   return (
@@ -29,6 +59,20 @@ function App() {
           <li key={product.id}>{product.name} - R$: {product.price}</li>
         ))}
       </ul>
+
+      <div className="add-product">
+        <form onSubmit={handleSubmit}>
+
+          <label>
+            Nome: <input type="text" name="name" value={name} onChange={(event) => setName(event.target.value)}/>
+          </label>
+
+          <label>
+            Preço: <input type="number" name="price" value={price} onChange={(event) => setPrice(event.target.value)}/>
+          </label>
+            <input type="submit" value="Criar" />
+        </form>
+      </div>
   </div>
   );
 }
