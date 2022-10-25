54307c5f8c8ec42e56177e94f19a18c9ee41fe90
clacerda <claudiovieira_jr@hotmail.com>
2022-10-25 20:20:17 -0300

    Validação de erro - API

--- Dev-API/index.js
+++ Dev-API/index.js
@@ -15,15 +15,22 @@ app.get('/', (req, res) => {
     res.json({message: 'Primeira rota criada com sucesso!'});
 });
 
+
+
 //rota post
 app.post('/createproduct', (req, res) => {
     const name = req.body.name
     const price = req.body.price
 
+    if(!name){
+        res.status(422).json({message: `O campo 'nome' é obrigatório`});
+        return;
+    }
+
     console.log(name)
     console.log(price)
 
-    res.json({message: `O produto ${name} foi cadastrado com sucesso no valor de R$${price}`})
+    res.status(200).json({message: `O produto ${name} foi cadastrado com sucesso no valor de R$${price}`})
 })
 
 app.listen(3000);
