bc11a6f6a6f40cbdafaef1c3c015c5734fff9c25
clacerda <claudiovieira_jr@hotmail.com>
2022-10-26 19:44:01 -0300

    aula 1 - typeScript

---
+++ INTRO_TYPESCRIPT/index.ts
@@ -0,0 +1,10 @@
+const firstName = "Claudio"
+const anotherName = 21
+const x = true
+
+function greeting(name : string){
+    console.log("Olá " + name)
+}
+
+greeting(firstName)
+greeting("anotherName") 
