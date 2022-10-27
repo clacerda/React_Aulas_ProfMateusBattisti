0fa0bfca6af2cafbcf98b24c8cea668b23a578cb
clacerda <claudiovieira_jr@hotmail.com>
2022-10-27 19:11:41 -0300

    Criando components com TS

---
+++ INTERMEDIARIO_TYPESCRIPT/11_react_com_ts/src/components/firtsComponents.tsx
@@ -0,0 +1,15 @@
+// 3- componente
+
+import React from 'react'
+
+type Props = {}
+
+function firtsComponents( ) {
+  return (
+    <div>
+        <h1>Meu primeiro componente</h1>
+    </div>
+  )
+}
+
+export default firtsComponents
