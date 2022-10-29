b3528e31e9ff8baf90a93a088dbe33b37a954b98
clacerda <claudiovieira_jr@hotmail.com>
2022-10-29 16:00:22 -0300

    Aula react rout

--- REACTROUTER/reactrouter/src/App.js
+++ REACTROUTER/reactrouter/src/App.js
@@ -2,10 +2,20 @@ import './App.css';
 //1 - config react rout.
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
+// pages import
+import Home from './pages/Home';
+import About from './pages/About';
+
 function App() {
   return (
     <div className="App">
         <h1>React Router</h1>
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+          </Routes>
+        </BrowserRouter>
     </div>
   );
 }
