968fe771e94530f40ec4672939c819829f4d7133
clacerda <claudiovieira_jr@hotmail.com>
2022-10-29 18:02:09 -0300

    Products

--- REACTROUTER/reactrouter/src/App.js
+++ REACTROUTER/reactrouter/src/App.js
@@ -2,15 +2,19 @@ import './App.css';
 //1 - config react rout.
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
+//Components
+
 // pages import
 import Home from './pages/Home';
 import About from './pages/About';
+import Navbar from './components/Navbar';
 
 function App() {
   return (
     <div className="App">
         <h1>React Router</h1>
         <BrowserRouter>
+        <Navbar />
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/about' element={<About />} />
