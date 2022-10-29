5bdf9ce6f1b75cb902c05da3744d8a35deb580bf
clacerda <claudiovieira_jr@hotmail.com>
2022-10-29 15:37:39 -0300

    Aula react rout

---
+++ REACTROUTER/reactrouter/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+//1 - config react rout.
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+        <h1>React Router</h1>
+    </div>
+  );
+}
+
+export default App;
