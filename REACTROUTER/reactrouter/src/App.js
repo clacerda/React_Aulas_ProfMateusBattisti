ce3a267b755a1d4ff9a3b303c0a6e67f5c5ef6bd
clacerda <claudiovieira_jr@hotmail.com>
2022-10-30 12:00:44 -0300

    Página genérica

--- REACTROUTER/reactrouter/src/App.js
+++ REACTROUTER/reactrouter/src/App.js
@@ -9,6 +9,8 @@ import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
 import Product from './pages/Product';
+import Info from './pages/Info';
+import NotFound from './pages/NotFound';
 
 function App() {
   return (
@@ -19,7 +21,14 @@ function App() {
           <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/about' element={<About />} />
-            <Route path='/Products/:id' element={<Product />} />
+            
+            <Route path='/products/:id' element={<Product />} />
+
+            {/* Nested Route */}
+            <Route path='/products/:id/info' element={<Info />} />
+            
+            {/* No mach route */}
+            <Route path='*' element={<NotFound />} />
           </Routes>
         </BrowserRouter>
     </div>
---
+++ REACTROUTER/reactrouter/src/pages/NotFound.js
@@ -0,0 +1,11 @@
+ 
+const NotFound = () => {
+  return (
+    <div>
+        <h1>404</h1>
+        <p>Nenhum conteúdo encontrado</p>
+    </div>
+  )
+}
+
+export default NotFound
