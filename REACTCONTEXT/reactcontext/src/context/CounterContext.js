ab7d5b14e08a5a7a775a6937405312542c96d3bd
clacerda <claudiovieira_jr@hotmail.com>
2022-10-31 20:40:18 -0300

    Consumindo context

--- REACTCONTEXT/reactcontext/src/context/CounterContext.js
+++ REACTCONTEXT/reactcontext/src/context/CounterContext.js
@@ -3,4 +3,16 @@ import {createContext, useState} from 'react';
 
  
 
-export default CounterContext = createContext();
+export const CounterContext = createContext();
+
+// criar provider
+
+export const CounterContextProvider = ({children}) => {
+    const [counter, setCounter] = useState(5);
+
+    return (
+        <CounterContext.Provider value={{counter, setCounter}}>
+            {children}
+        </CounterContext.Provider>
+    )
+}
