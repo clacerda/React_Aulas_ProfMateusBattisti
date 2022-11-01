f497f28c90073dd577b6b1aa70dd647eb25bb245
clacerda <claudiovieira_jr@hotmail.com>
2022-11-01 20:50:56 -0300

    context - count

--- REACTCONTEXT/reactcontext/src/pages/Home.js
+++ REACTCONTEXT/reactcontext/src/pages/Home.js
@@ -1,11 +1,15 @@
 
-import { useContext } from "react"
+// import { useContext } from "react"
 import ChangeCount from "../components/ChangeCount"
-import {CounterContext} from "../context/CounterContext"
+// import {CounterContext} from "../context/CounterContext"
+
+// 4 refatorando hook
+import { useCounterContext } from "../hooks/useCounterContext"
 
 const Home = () => {
 
-  const {counter} = useContext(CounterContext)
+//  const {counter} = useContext(CounterContext)
+const {counter} = useCounterContext();
 
   return (
     <div>
