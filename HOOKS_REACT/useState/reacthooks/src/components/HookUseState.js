ba59af6ffba1690ff3c8427055c8e77d9b621ecb
clacerda <claudiovieira_jr@hotmail.com>
2022-11-14 19:28:55 -0300

    useState - aula 1

--- HOOKS_REACT/useState/reacthooks/src/components/HookUseState.js
+++ HOOKS_REACT/useState/reacthooks/src/components/HookUseState.js
@@ -4,13 +4,18 @@ const HookUseState = () => {
 
  //1 - useState
  let userName = "João"
-
+ 
  const [name, setName] = useState("Claudio")
 
  const ChangeName = () => {
     userName = "João souza"
     setName("Betoneira")
  }
+
+  //2 - useState e input
+  const [age, setAge] = useState(18);
+
+
 
   return (
     <div>
@@ -20,6 +25,13 @@ const HookUseState = () => {
         <p>useState: {name}</p>
         <button onClick={ChangeName}>Mudar nome</button>
         <hr />
+
+        {/* UseState e Input */}
+        <p>Digite a sua idade:</p>
+        <form>
+            <input type="text"   id="" value={age} onChange={(e) => setAge(e.target.value)} />
+            <p>Você tem: {age} anos!</p>
+        </form>
     </div>
   )
 }
