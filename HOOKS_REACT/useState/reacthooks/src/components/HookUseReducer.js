7fea43a62499395cdd4018e03f896c36498530a8
clacerda <claudiovieira_jr@hotmail.com>
2022-11-15 15:58:34 -0300

    Aula 2 - useReducer

--- HOOKS_REACT/useState/reacthooks/src/components/HookUseReducer.js
+++ HOOKS_REACT/useState/reacthooks/src/components/HookUseReducer.js
@@ -1,4 +1,4 @@
-import {useReducer} from 'react'
+import {useReducer, useState} from 'react'
 
 const HookUseReducer = () => {
 
@@ -8,6 +8,35 @@ const HookUseReducer = () => {
         return Math.random(state);
     });
 
+    // 2- avançando no useReducer
+    const initialTasks  = [
+        {id: 1, text: "texto 1"},
+        {id: 2, text: "texto 2"},
+    ]
+
+    const taskReducer = (state, action) => {
+        switch(action.type){
+            case "ADD":
+                const newTask = {
+                    id: Math.random(),
+                    text: taskTeste,
+                };
+
+                setTaskTest("");
+            return [...state, newTask];
+        }
+    }
+
+    const [taskTeste, setTaskTest] = useState("");
+    const [tasks, dispatchTasks] = useReducer(taskReducer, initialTasks);
+    
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        dispatchTasks({type: "ADD"});
+    }
+
   return (
     <div>
         <h2>
@@ -15,6 +44,15 @@ const HookUseReducer = () => {
         </h2>
         <p>Número: {number}</p>
         <button onClick={dispatch}>Alterar número</button>
+        
+        <h3>Tarefas</h3>
+        <form onSubmit={handleSubmit}>
+            <input type="text"  id=""  onChange={(e) => setTaskTest(e.target.value)} value={taskTeste}/>
+            <input type="submit" value="Enviar" />
+        </form>
+        {tasks.map((task) => (
+            <li key={task.id}>{task.text}</li>
+        ))}
         <hr />
     </div>
   )
